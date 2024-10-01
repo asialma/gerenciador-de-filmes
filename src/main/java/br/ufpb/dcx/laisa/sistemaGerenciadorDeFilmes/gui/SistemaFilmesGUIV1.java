02e9b9407d18a2e2ecba350e76026128767b4da2
laisa.maria <laisa.maria@dcx.ufpb.br>
2024-10-01 09:40:16 -0300

    Implementando informações na interface

--- src/main/java/br/ufpb/dcx/laisa/sistemaGerenciadorDeFilmes/gui/SistemaFilmesGUIV1.java
+++ src/main/java/br/ufpb/dcx/laisa/sistemaGerenciadorDeFilmes/gui/SistemaFilmesGUIV1.java
@@ -3,48 +3,49 @@ package br.ufpb.dcx.laisa.sistemaGerenciadorDeFilmes.gui;
 import br.ufpb.dcx.laisa.sistemaGerenciadorDeFilmes.*;
 import br.ufpb.dcx.laisa.sistemaGerenciadorDeFilmes.controller.SisFilmesSearchController;
 import br.ufpb.dcx.laisa.sistemaGerenciadorDeFilmes.controller.SisFilmesRemoveController;
+import br.ufpb.dcx.laisa.sistemaGerenciadorDeFilmes.exceptions.FilmeJaCadastradoException;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Locale;
 
 public class SistemaFilmesGUIV1 extends JFrame {
     JLabel linha1, linha2;
-    SistemaFilmes sistemaFilmes = new SistemaFilmesMap();
+    SistemaFilmesMap sistemaFilmes = new SistemaFilmesMap();
+    ImageIcon capa = new ImageIcon("./imgs/capawatchlist.png");
     JMenuBar barraDeMenu = new JMenuBar();
 
     public SistemaFilmesGUIV1(){
         setTitle("Meu gerenciador de filmes");
-        setSize(800, 600); //tamanho da janela
+        setSize(800, 600);
         setLocation(150, 150);
         setResizable(false);
         setBackground(Color.white);
         linha1 = new JLabel("MyFilmes", JLabel.CENTER);
         linha1.setForeground(Color.red);
         linha1.setFont(new Font("Serif", Font.BOLD, 24));
+        linha2 = new JLabel(capa, JLabel.CENTER);
         setLayout(new GridLayout(3, 1));
         add(linha1);
         add(linha2);
         add(new JLabel());
 
         JMenu menuCadastrar = new JMenu("Cadastrar novo filme");
-        JMenuItem menuCadastrarFilme = new JMenuItem(
-                "Cadastrar filme");
+        JMenuItem menuCadastrarFilme = new JMenuItem("Cadastrar filme");
         menuCadastrar.add(menuCadastrarFilme);
 
         JMenu menuPesquisarID = new JMenu("Pesquisar filme");
-        JMenuItem menuPesquisarFilmeID = new JMenuItem(
-                "Pesquisar filme pelo ID");
+        JMenuItem menuPesquisarFilmeID = new JMenuItem("Pesquisar filme pelo ID");
         menuPesquisarID.add(menuPesquisarFilmeID);
 
         JMenu menuRemover = new JMenu("Remover filme");
-        JMenuItem menuRemoverFilme = new JMenuItem(
-                "Remover filme");
+        JMenuItem menuRemoverFilme = new JMenuItem("Remover filme");
         menuRemover.add(menuRemoverFilme);
 
         menuPesquisarFilmeID.addActionListener(
                 new SisFilmesSearchController(sistemaFilmes,this));
         menuRemoverFilme.addActionListener(
                 new SisFilmesRemoveController(sistemaFilmes, this));
-
         menuCadastrarFilme.addActionListener(
                 (ae) -> {
                     String id = JOptionPane.showInputDialog(this,
@@ -56,28 +57,17 @@ public class SistemaFilmesGUIV1 extends JFrame {
                     int anoLancamento = Integer.parseInt(JOptionPane.showInputDialog(this,
                             "Qual o ano de lançamento do filme?"));
 
-                    CategoriaDeFilmes[] categorias = CategoriaDeFilmes.values();
-                    CategoriaDeFilmes categoria = (CategoriaDeFilmes) JOptionPane.showInputDialog(this,
-                            "Escolha a categoria do filme:",
-                            "Categoria",
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            categorias,
-                            categorias[0]);
+                    String categoria = JOptionPane.showInputDialog(this,
+                            "Qual a categoria do filme?");
 
                     String nomeDiretor = JOptionPane.showInputDialog(this, "Qual o nome do diretor?");
                     int anoNascimentoDiretor = Integer.parseInt(JOptionPane.showInputDialog(this, "Qual o ano de nascimento do diretor?"));
                     Diretor diretor = new Diretor(nomeDiretor, anoNascimentoDiretor);
 
-
-                    boolean cadastrou = sistemaFilmes.cadastrarFilme(id,  titulo,  anoLancamento, categoria, diretor);
-                    if (cadastrou) {
-                        JOptionPane.showMessageDialog(this,
-                                "Aniversariante cadastrado");
-                    } else {
-                        JOptionPane.showMessageDialog(this,
-                                "Aniversariante não foi cadastrado. " +
-                                        "Verifique se já não existia");
+                    try {
+                        sistemaFilmes.cadastrarFilme(id,  titulo,  anoLancamento, categoria, diretor);
+                    } catch (FilmeJaCadastradoException e) {
+                        throw new RuntimeException(e);
                     }
                 });
 
